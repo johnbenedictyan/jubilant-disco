90193da5846e28bd66e181735c35620c31e31609
John Benedict Yan <johnbenedictyan@gmail.com>
2023-06-20 13:57:50 +0800

    Update queue viewer

--- src/view/queueViewer.ts
+++ src/view/queueViewer.ts
@@ -1,10 +1,21 @@
-import { Queue, User } from "@prisma/client";
+import { Queue, QueueItem, User } from "@prisma/client";
 
-export default function queueViewer(queue: Queue, currentUser?: User) {
+type FullQueue = Queue & {
+  queueItemList: QueueItem[];
+  _count: { queueItemList: number };
+};
+
+export default function queueViewer(queue: FullQueue, currentUser?: User) {
+  const queueItemListView = queue.queueItemList.sort(
+    (qi1, qi2) => qi1.number - qi2.number
+  );
+
   const queueView = {
     hash: queue.hash,
     name: queue.name,
     shopId: queue.shopId,
+    queueItemList: queueItemListView,
+    queueItemCount: queue._count.queueItemList,
   };
   return queueView;
 }
